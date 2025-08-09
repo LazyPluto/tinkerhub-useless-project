9a76fa7bd3afe3a470a67190518be53136be1890
LazyPluto <LazyPluto@users.noreply.github.com>
2025-08-09 14:04:15 +0530

    More work done.

--- src/lib/components/Keyboard.svelte
+++ src/lib/components/Keyboard.svelte
@@ -1,21 +1,39 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 
-	let pressedKey = null;
+	let { keymap } = $props();
+
 	let keys = [
 		['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
 		['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-		['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+		['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',']
 	];
 
-	// Function to handle the key press event
-	function handleKeyPress(event) {
-		pressedKey = event.key.toUpperCase();
+	let keyStates = $state(createKeyStates(keys));
+
+	function createKeyStates(keys: string[][]): Map<string, boolean> {
+		const map = new Map<string, boolean>();
+		keys.forEach((innerArray) => {
+			innerArray.forEach((char) => {
+				map.set(char, false);
+			});
+		});
+		return map;
 	}
 
-	// Listen to keydown events
+	function handleKeyPress(event: KeyboardEvent) {
+		keyStates.set(event.key.toUpperCase(), true);
+		keyStates = new Map(keyStates);
+	}
+
+	function handleKeyRelease(event: KeyboardEvent) {
+		keyStates.set(event.key.toUpperCase(), false);
+		keyStates = new Map(keyStates);
+	}
+
 	onMount(() => {
 		window.addEventListener('keydown', handleKeyPress);
+		window.addEventListener('keyup', handleKeyRelease);
 		return () => {
 			window.removeEventListener('keydown', handleKeyPress);
 		};
@@ -26,8 +44,8 @@
 	{#each keys as row}
 		<div class="keyrow">
 			{#each row as key}
-				<div class="key">
-					{key}
+				<div class="key {keyStates.get(key) ? 'pressed' : ''}">
+					{keymap.get(key)}
 				</div>
 			{/each}
 		</div>
@@ -38,8 +56,8 @@
 	.keyboard {
 		display: flex;
 		flex-direction: column;
+		gap: 0.3em;
 		max-width: 900px;
-		margin: auto;
 		text-align: center;
 		align-items: center;
 	}
@@ -48,23 +66,26 @@
 		display: flex;
 		flex-direction: row;
 		align-items: center;
+		gap: 0.4em;
 	}
 
 	.key {
-		aspect-ratio: 1 / 1;
-		padding: 10px;
-		margin: 5px;
-		border: 2px solid #ccc;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 3em;
+		height: 3em;
+		border: 2px solid #6c7086;
 		border-radius: 4px;
-		background-color: #f0f0f0;
+		background-color: #313244;
 		cursor: pointer;
-		transition: background-color 0.3s;
-		font-size: 14px;
-		font-family: 'Courier New', monospace;
+		transition: background-color 0.15s;
+		font-size: 16pt;
+		font-family: 'Roboto Mono', monospace;
 	}
 
 	.key.pressed {
-		background-color: #4caf50;
+		background-color: #b4befe;
 		color: white;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,112 +1,232 @@
-<script>
+<script lang="ts">
 	import Keyboard from '$lib/components/Keyboard.svelte';
+	import { onMount } from 'svelte';
 
-	let sentence = 'The quick brown fox jumps over the lazy dog';
-	let typedText = '';
-	let startTime;
-	let endTime;
-	let isTestStarted = false;
-	let isTestFinished = false;
-	let timeTaken = 0;
-	let wpm = 0;
+	const TestState = {
+		NOT_STARTED: 'NOT_STARTED',
+		RUNNING: 'RUNNING',
+		ENDED: 'ENDED'
+	};
 
-	// Start a new test
+	let words = prepareWords(
+		'The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog'
+	);
+	let letterStates = $state(prepareStates(words));
+	// let keymap = $state(prepareDefaultKeymap(0.5));
+	let keymap = $state(prepareABCDKeymap());
+	let timeLeft = $state(0);
+	let nextWord = 0;
+	let nextChar = 0;
+	let lettersTyped = 0;
+	let startTime: Date;
+	let endTime: Date;
+	let testState = $state(TestState.NOT_STARTED);
+	let wpm = $state(0);
+	let timer: number;
+
+	function prepareWords(sentence: string): string[][] {
+		const words = sentence.toLowerCase().split(' ');
+		const result = words.map((word) => word.split(''));
+		return result;
+	}
+
+	function prepareStates(words: string[][]): boolean[][] {
+		return words.map((word) => word.map((_) => false));
+	}
+
+	function prepareDefaultKeymap(difficulty: number): Map<string, string> {
+		const letters = [];
+		for (let i = 65; i <= 90; i++) {
+			letters.push(i);
+		}
+
+		// for (let i = letters.length - 1; i > 0; i--) {
+		// 	const j = Math.floor(Math.random() * (i + 1));
+		// 	[letters[i], letters[j]] = [letters[j], letters[i]];
+		// }
+
+		const rounds = Math.round(letters.length * difficulty);
+		for (let round = 0; round < rounds; round++) {
+			const i = Math.floor(Math.random() * letters.length);
+			const j = Math.floor(Math.random() * (i + 1));
+			[letters[i], letters[j]] = [letters[j], letters[i]];
+		}
+
+		const letterMap = new Map<string, string>();
+
+		for (let i = 0; i < letters.length; i++) {
+			letterMap.set(String.fromCharCode(65 + i), String.fromCharCode(letters[i]));
+		}
+
+		return letterMap;
+	}
+
+	function prepareABCDKeymap(): Map<string, string> {
+		const letters = [
+			'Q',
+			'W',
+			'E',
+			'R',
+			'T',
+			'Y',
+			'U',
+			'I',
+			'O',
+			'P',
+			'A',
+			'S',
+			'D',
+			'F',
+			'G',
+			'H',
+			'J',
+			'K',
+			'L',
+			'Z',
+			'X',
+			'C',
+			'V',
+			'B',
+			'N',
+			'M'
+		];
+
+		const letterMap = new Map<string, string>();
+		for (let i = 0; i < letters.length; i++) {
+			letterMap.set(letters[i], String.fromCharCode(65 + i));
+		}
+		return letterMap;
+	}
+
 	function startTest() {
-		typedText = '';
-		isTestStarted = true;
-		isTestFinished = false;
+		console.log('Starting test');
+		testState = TestState.RUNNING;
 		startTime = new Date();
+		endTime = new Date(startTime.getTime() + 30 * 1000);
+		timer = setInterval(updateWPM, 1000);
+	}
+
+	function stopTest() {
+		console.log('Stopping test');
+		testState = TestState.ENDED;
+		clearInterval(timer);
+	}
+
+	function updateWPM() {
+		const time = new Date();
+		const timeTaken = (time - startTime) / 1000;
+		const wordsTyped = lettersTyped / 5;
+		wpm = Math.round((wordsTyped / timeTaken) * 60);
+		timeLeft = Math.floor(120 - timeTaken);
+
+		if (timeTaken >= 120) stopTest();
 	}
 
 	// Update the typed text and check if the test is finished
-	function handleTyping(event) {
-		typedText = event.target.value;
+	function handleTyping(event: KeyboardEvent) {
+		let key = event.key.toUpperCase();
+		key = keymap.get(key)?.toLowerCase();
 
-		if (typedText === sentence) {
-			endTime = new Date();
-			isTestFinished = true;
-			timeTaken = (endTime - startTime) / 1000; // in seconds
-			wpm = Math.round((sentence.split(' ').length / timeTaken) * 60);
+		if (event.key == ' ') {
+			if (nextChar != 0) {
+				nextWord++;
+				nextChar = 0;
+			}
+		} else {
+			if (nextChar < words[nextWord].length) {
+				if (words[nextWord][nextChar] === key) {
+					letterStates[nextWord][nextChar] = true;
+					lettersTyped++;
+					nextChar++;
+				}
+			}
 		}
 	}
 
 	// Reset the test
 	function resetTest() {
-		typedText = '';
-		isTestStarted = false;
-		isTestFinished = false;
-		timeTaken = 0;
+		testState = TestState.NOT_STARTED;
 		wpm = 0;
 	}
+
+	onMount(() => {
+		window.addEventListener('keydown', (event) => {
+			if (testState === TestState.NOT_STARTED) startTest();
+			if (testState === TestState.RUNNING) handleTyping(event);
+		});
+	});
 </script>
 
 <div class="test-container">
 	<h1>Typing Speed Test</h1>
 
-	{#if !isTestStarted}
-		<p>Click below to start the test.</p>
-		<button on:click={startTest}>Start Test</button>
-	{/if}
+	<div class="stats {testState === TestState.NOT_STARTED ? 'hidden' : ''}">
+		<p>{timeLeft}s</p>
+		<p>WPM: {wpm}</p>
+	</div>
 
-	{#if isTestStarted}
-		<p class="sentence">{sentence}</p>
-		<input type="text" bind:value={typedText} on:input={handleTyping} disabled={isTestFinished} />
-	{/if}
-
-	{#if isTestFinished}
-		<div class="result">
-			<p>Time Taken: {timeTaken} seconds</p>
-			<p>Words per Minute: {wpm} WPM</p>
-			<button on:click={resetTest}>Try Again</button>
-		</div>
-	{/if}
-
-	<Keyboard />
+	<div class="words">
+		{#each words as word, wordIndex}
+			<div class="word">
+				{#each word as char, charIndex}
+					<div class="char {letterStates[wordIndex][charIndex] ? 'typed' : ''}">{char}</div>
+				{/each}
+			</div>
+		{/each}
+	</div>
+	<Keyboard {keymap} />
 </div>
 
 <style>
 	.test-container {
-		width: 100%;
-		max-width: 600px;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 		margin: auto;
 		text-align: center;
 		padding: 20px;
 		font-family: Arial, sans-serif;
+		background-color: #1e1e2e;
+		color: #cdd6f4;
 	}
 
-	.sentence {
-		font-size: 1.5rem;
+	.stats {
+		width: 75%;
+		margin-top: 8%;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		font-size: 16pt;
+	}
+
+	.hidden {
+		opacity: 0;
+	}
+
+	.words {
+		width: 75%;
+		height: 40%;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		align-content: flex-start;
+		overflow: hidden;
+		gap: 0.5rem 1rem;
+		font-size: 2.5rem;
 		margin-bottom: 20px;
-		padding: 10px;
-		background: #f3f3f3;
+		background: transparent;
 		border-radius: 4px;
+		color: #45475a;
 	}
 
-	input {
-		width: 100%;
-		padding: 10px;
-		font-size: 1.2rem;
-		margin-top: 10px;
-		border: 2px solid #ccc;
-		border-radius: 4px;
+	.word {
+		display: flex;
+		flex-direction: row;
 	}
 
-	.result {
-		margin-top: 20px;
-	}
-
-	button {
-		margin-top: 20px;
-		padding: 10px 20px;
-		font-size: 1rem;
-		cursor: pointer;
-		border: none;
-		background-color: #4caf50;
-		color: white;
-		border-radius: 4px;
-	}
-
-	button:hover {
-		background-color: #45a049;
+	.char.typed {
+		color: #cdd6f4;
 	}
 </style>
